c33af3841ff7fdd1f085cc81b4764e1a534e629b
J C M <jcmeijers@gmail.com>
2016-06-07 09:56:35 +0000

    Tweaked style.less, functions.php. Added header bar.

--- functions.php
+++ functions.php
@@ -30,40 +30,44 @@ function beans_child_enqueue_assets() {
 
 }
 
-// Replace site title tag styling
-beans_replace_attribute( 'beans_site_title_tag', 'class', 'uk-text-muted', 'uk-text-center uk-margin-top uk-text-break uk-text-success' );
-
-// beans_modify_action_hook( 'beans_post_image', 'beans_post_title_before_markup' );
-
-// Removing the site branding.
+// Remove Site Title Tag
 beans_remove_action( 'beans_site_title_tag' );
 
+// Remove Active from Breadcrumbs
+beans_add_attribute( 'beans_breadcrumb_item[_active]', 'class', 'uk-hidden' );
+
+// Modify Beans Main.
+beans_add_attribute( 'beans_main', 'class', 'uk-padding-remove' );
+// beans_add_attribute( 'beans_main', 'class', 'uk-padding-top-remove' );
+// beans_add_attribute( 'beans_main', 'class', 'uk-margin-bottom-remove' );
+
+// Add Back to Top.
+add_action( 'beans_body_prepend_markup', 'back_to_top_anchor' );
+
+function back_to_top_anchor() {
+
+    ?><div id="blue-top"></div><?php
+
+}
+
+// Modify Beans Widget Title.
+beans_modify_markup( 'beans_widget_title', 'h4' );
+
 // Add Header Bar
-beans_add_action( 'beans_header_before_markup', 'header_bar' );
-
-// Modifying site title tag location
-beans_modify_action_hook( 'beans_site_title_tag', 'header_bar_before_markup' );
-
-// Style beans_site_title_tag
-function header_bar() {
-
-    ?>
-    <div class="tm-header-bar uk-block-muted">
-
-        <div class="uk-container uk-container-center uk-text-justify">
-
-            <div class="uk-grid" data-uk-grid-match data-uk-margin>
-                <div class="uk-width-large-1-2 uk-width-medium-1-2 uk-text-primary uk-text-middle uk-text-left uk-text-center-small uk-text-break">
-                    <?php echo beans_site_title_tag(); ?>
-                </div>
-                <div class="uk-width-large-1-2 uk-width-medium-1-2 uk-text-right uk-text-middle uk-text-center-small">
-                    <a href="tel:1-408-555-5555">Call now to get your beans!</a>
-                </div>
-            </div>
-
-        </div>
-
-    </div>
-    <?php
-
+add_action( 'beans_header_before_markup', 'blue_header_bar' );
+function blue_header_bar() {
+	?>
+	<div class="blue-header-bar uk-block-muted">
+		<div class="uk-container uk-container-center uk-text-justify">
+			<div class="uk-grid uk-contrast" data-uk-grid-match data-uk-margin>
+    			<div class="uk-width-large-1-2 uk-width-medium-1-1 uk-text-left uk-text-middle uk-text-center-medium uk-text-center-small uk-hidden-medium uk-hidden-small">
+    			    <?php beans_site_title_tag(); ?>
+    			</div>
+    			<div class="uk-width-large-1-2 uk-width-medium-1-1 uk-text-right uk-text-middle uk-text-center-medium uk-text-center-small">
+    				<a href="#"><i class="uk-icon-map-marker"></i> Locaties</a> <a href="mailto:info@mail.net"><i class="uk-icon-envelope"></i> info @ mail.net</a> <a href="tel:1-562-867-5309"><i class="uk-icon-phone-square"></i> 562-867-5309</a>
+    			</div>
+			</div>
+		</div>
+	</div>
+	<?php
 }
